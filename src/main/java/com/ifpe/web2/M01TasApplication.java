de0df9e2f5046c6bef7f1cefd5d842edfd2c2cc9
aluno <aluno@ln179860>
2019-11-27 14:24:07 -0300

    segundo commit

--- src/main/java/com/ifpe/web2/M01TasApplication.java
+++ src/main/java/com/ifpe/web2/M01TasApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class M01TasApplication {
-
+//teste 
 	public static void main(String[] args) {
 		SpringApplication.run(M01TasApplication.class, args);
 	}
